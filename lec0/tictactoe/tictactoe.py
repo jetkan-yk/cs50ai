ed2426eba66d87adaac61ac100ead2d559b6cbb4
Jet Kan <jetkan.yk@gmail.com>
2021-07-28 13:29:32 +0800

    Implement actions

--- lec0/tictactoe/tictactoe.py
+++ lec0/tictactoe/tictactoe.py
@@ -30,7 +30,14 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    result = set()
+
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == EMPTY:
+                result.add((i, j))
+
+    return result
 
 
 def result(board, action):
