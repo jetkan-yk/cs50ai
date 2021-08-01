ceb708dd300bf826d06e4b24d810cc0108e89350
Jet Kan <jetkan.yk@gmail.com>
2021-08-01 21:39:47 +0800

    Implement sentence

--- lec1/minesweeper/minesweeper.py
+++ lec1/minesweeper/minesweeper.py
@@ -104,27 +104,32 @@ class Sentence:
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells
+        return set()
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
+        return set()
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
+        self.cells.remove(cell)
+        self.count -= 1
 
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        self.cells.remove(cell)
 
 
 class MinesweeperAI:
