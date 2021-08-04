fb4005200d87c8946e5fa132fde6efc8116a4b26
Jet Kan <jetkan.yk@gmail.com>
2021-08-04 09:13:37 +0800

    Add degrees test

---
+++ lec0/degrees/test_degrees.py
@@ -0,0 +1,39 @@
+from degrees import *
+
+load_data("large")
+
+
+def test_zero_degree():
+    source = person_id_for_name("Tim Zinnemann")
+    target = person_id_for_name("Lahcen Zinoun")
+    assert shortest_path(source, target) == None
+
+
+def test_one_degree():
+    source = person_id_for_name("Tom Cruise")
+    target = person_id_for_name("Lea Thompson")
+    assert len(shortest_path(source, target)) == 1
+
+
+def test_two_degree():
+    source = person_id_for_name("Tom Cruise")
+    target = person_id_for_name("Tom Hanks")
+    assert len(shortest_path(source, target)) == 2
+
+
+def test_three_degree():
+    source = person_id_for_name("Emma Watson")
+    target = person_id_for_name("Jennifer Lawrence")
+    assert len(shortest_path(source, target)) == 3
+
+
+def test_four_degree():
+    source = person_id_for_name("Fred Astaire")
+    target = person_id_for_name("Mohamed Zinet")
+    assert len(shortest_path(source, target)) == 4
+
+
+def test_eight_degree():
+    source = person_id_for_name("Juliane Banse")
+    target = person_id_for_name("Julian Acosta")
+    assert len(shortest_path(source, target)) == 8
