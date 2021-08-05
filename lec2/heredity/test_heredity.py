4496719f2c9d94751e238f9eaf9c39b9a3078e85
Jet Kan <jetkan.yk@gmail.com>
2021-08-05 20:32:39 +0800

    Add tests

--- lec2/heredity/test_heredity.py
+++ lec2/heredity/test_heredity.py
@@ -1,8 +1,7 @@
 from pprint import pp
 from random import randint, random
 
-from pytest import approx
-
+from helper import compare, predict_family
 from heredity import *
 
 
@@ -29,7 +28,7 @@ def test_normalize():
 
     for person in probabilities.values():
         for variable in person.values():
-            assert sum(variable.values()) == approx(1)
+            assert round(sum(variable.values())) == 1
 
 
 def test_update():
@@ -115,3 +114,86 @@ def test_get_gene():
 
     for person in people:
         assert get_gene(person, {"James"}, {"Lily"}) == expected[person]
+
+
+def test_family0():
+    expected = {
+        "Harry": {
+            "gene": {2: 0.0092, 1: 0.4557, 0: 0.5351},
+            "trait": {True: 0.2665, False: 0.7335},
+        },
+        "James": {
+            "gene": {2: 0.1976, 1: 0.5106, 0: 0.2918},
+            "trait": {True: 1.0000, False: 0.0000},
+        },
+        "Lily": {
+            "gene": {2: 0.0036, 1: 0.0136, 0: 0.9827},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+    }
+
+    predicted = predict_family(0)
+
+    compare(predicted, expected)
+
+
+def test_family1():
+    expected = {
+        "Arthur": {
+            "gene": {2: 0.0329, 1: 0.1035, 0: 0.8636},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Charlie": {
+            "gene": {2: 0.0018, 1: 0.1331, 0: 0.8651},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Fred": {
+            "gene": {2: 0.0065, 1: 0.6486, 0: 0.3449},
+            "trait": {True: 1.0000, False: 0.0000},
+        },
+        "Ginny": {
+            "gene": {2: 0.0027, 1: 0.1805, 0: 0.8168},
+            "trait": {True: 0.1110, False: 0.8890},
+        },
+        "Molly": {
+            "gene": {2: 0.0329, 1: 0.1035, 0: 0.8636},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Ron": {
+            "gene": {2: 0.0027, 1: 0.1805, 0: 0.8168},
+            "trait": {True: 0.1110, False: 0.8890},
+        },
+    }
+
+    predicted = predict_family(1)
+
+    compare(predicted, expected)
+
+
+def test_family2():
+    expected = {
+        "Arthur": {
+            "gene": {2: 0.0147, 1: 0.0344, 0: 0.9509},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Hermione": {
+            "gene": {2: 0.0608, 1: 0.1203, 0: 0.8189},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Molly": {
+            "gene": {2: 0.0404, 1: 0.0744, 0: 0.8852},
+            "trait": {True: 0.0768, False: 0.9232},
+        },
+        "Ron": {
+            "gene": {2: 0.0043, 1: 0.2149, 0: 0.7808},
+            "trait": {True: 0.0000, False: 1.0000},
+        },
+        "Rose": {
+            "gene": {2: 0.0088, 1: 0.7022, 0: 0.2890},
+            "trait": {True: 1.0000, False: 0.0000},
+        },
+    }
+
+    predicted = predict_family(2)
+
+    compare(predicted, expected)
