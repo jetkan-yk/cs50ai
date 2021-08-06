b67b2595ea06e54fe8be57ac039a7ced36bd577c
Jet Kan <jetkan.yk@gmail.com>
2021-08-06 21:33:00 +0800

    Add degrees tests

--- lec0/degrees/degrees_reg_test.py
+++ lec0/degrees/degrees_reg_test.py
@@ -1,5 +1,10 @@
 """
 Regression tests for degrees.py
+
+Make sure that you placed this file in the same directory as degrees.py!
+
+'Why do we fall sir? So that we can learn to pick ourselves up.'
+                                        - Batman Begins (2005)
 """
 from degrees import load_data, person_id_for_name, shortest_path
 
