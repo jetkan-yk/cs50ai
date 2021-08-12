6b6c55f2db111b99641b70207823337d358f5952
Jet Kan <jetkan.yk@gmail.com>
2021-08-12 15:31:58 +0800

    Add exit code

--- test.bat
+++ test.bat
@@ -1,5 +1,10 @@
+setlocal ENABLEDELAYEDEXPANSION
+
 @echo off
 for /d /r %%d in (*) do (
     cd %%d
-    if exist *_test.py pytest
+    if exist *_test.py (
+        pytest 
+        if !errorlevel! neq 0 exit /b 1
+    )
 )
