14eaaa6b3d21b7e589f425de803e66ba7669c2da
Jet Kan <jetkan.yk@gmail.com>
2021-08-14 21:05:19 +0800

    Add shopping unit test

---
+++ lec4/shopping/shopping_unit_test.py
@@ -0,0 +1,18 @@
+"""
+Unit tests for functions in shopping.py
+
+These tests will only work if your own version of shopping.py
+also happen to have implemented the same functions as mine!
+Feel free to replace the unit test functions with your version.
+
+'Why do we fall sir? So that we can learn to pick ourselves up.'
+                                        - Batman Begins (2005)
+"""
+from shopping import load_data
+
+
+def test_load_data():
+    evidence, labels = load_data("shopping.csv")
+    assert len(evidence) == len(labels)
+    assert evidence[0] == [0, 0.0, 0, 0.0, 1, 0.0, 0.2, 0.2, 0.0, 0.0, 1, 1, 1, 1, 1, 1, 0]
+    assert labels[0] == 0
