cf4e05269988245b10b7443d73515cb5c7b0c422
Jet Kan <jetkan.yk@gmail.com>
2021-08-15 18:31:30 +0800

    Update test

--- lec4/shopping/shopping_test.py
+++ lec4/shopping/shopping_test.py
@@ -9,9 +9,13 @@ Make sure that this file is in the same directory as shopping.py!
 
 import csv
 
+import numpy as np
+
 from shopping import TEST_SIZE, evaluate, load_data, train_model, train_test_split
 
 FILENAME = "shopping.csv"
+
+expected0 = [0, 0.0, 0, 0.0, 1, 0.0, 0.2, 0.2, 0.0, 0.0, 1, 1, 1, 1, 1, 1, 0]
 
 
 def test():
@@ -31,6 +35,11 @@ def test():
 
     evidence, labels = load_data(FILENAME)
     assert len(evidence) == len(labels) == N
+    if type(evidence).__module__ == np.__name__:
+        assert (evidence[0] == expected0).all()
+    else:
+        assert evidence[0] == expected0
+    assert labels[0] == 0
 
     X_train, X_test, y_train, y_test = train_test_split(
         evidence, labels, test_size=TEST_SIZE
