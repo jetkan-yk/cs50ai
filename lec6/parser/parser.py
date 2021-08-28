a65be8a893494c12f51fddd9342b84965867f207
Jet Kan <jetkan.yk@gmail.com>
2021-08-28 17:50:51 +0800

    Add np_chunk

--- lec6/parser/parser.py
+++ lec6/parser/parser.py
@@ -77,7 +77,8 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    # TODO: Update
+    return [subtree for subtree in tree.subtrees(lambda t: t.label() == "NP")]
 
 
 if __name__ == "__main__":
