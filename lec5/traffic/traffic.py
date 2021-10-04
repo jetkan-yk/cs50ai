7ac953b1d67b48b1cd3b8409585410a20c7ac080
Jet Kan <jetkan.yk@gmail.com>
2021-10-04 21:33:13 +0800

    traffic v2

--- lec5/traffic/traffic.py
+++ lec5/traffic/traffic.py
@@ -92,7 +92,7 @@ def get_model():
         [
             # Convolutional layer. Learn 32 filters using a 3x3 kernel
             Conv2D(
-                32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+                64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
             ),
             # Max-pooling layer, using 2x2 pool size
             MaxPooling2D(pool_size=(2, 2)),
